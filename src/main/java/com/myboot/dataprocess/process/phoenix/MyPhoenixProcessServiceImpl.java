691065b4c26a6698ab664dbc19d09089e47636c9
frake2009 <frake2009@163.com>
2019-10-12 12:11:19 +0800

    完善代码

--- src/main/java/com/myboot/dataprocess/process/phoenix/MyPhoenixProcessServiceImpl.java
+++ src/main/java/com/myboot/dataprocess/process/phoenix/MyPhoenixProcessServiceImpl.java
@@ -4,7 +4,10 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class MyPhoenixProcessServiceImpl implements MyPhoenixProcessService{
 	
 	
